b6284d4e947c4e44102d2ed1769e4870ad577bf8
udaymondal <udaysk.mondal@gmail.com>
2021-03-21 21:13:33 +0600

    1p

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        const { displayName, email, photoURL } = res.user;
+        // console.log(displayName, email, photoURL);
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.message);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSignIn}>Sign-in</button>
+    {
+      user.isSignedIn && <div>
+        <p>Welcome, {user.name}</p>
+        <p>Email: {user.email}</p>
+        <img src={user.photo} alt=""/>
+      </div>
+    }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6zuv2xtuiKRg1wAmcJNsLtkdDQAsSTnA",
+    authDomain: "ema-john-simple-ph1.firebaseapp.com",
+    projectId: "ema-john-simple-ph1",
+    storageBucket: "ema-john-simple-ph1.appspot.com",
+    messagingSenderId: "1047840763185",
+    appId: "1:1047840763185:web:7f4a4dfbcebef4fd7e1410"
+};
+
+export default firebaseConfig;
