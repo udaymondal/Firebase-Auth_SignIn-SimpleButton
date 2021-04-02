d79a777c41464de2c4464f3c4aa6ce45f6db9c82
udaymondal <udaysk.mondal@gmail.com>
2021-04-02 21:07:41 +0600

    module 42-3 finished

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
       })
   }
 
-  const handleSignOut = () =>{
+  const handleSignOut = () => {
     firebase.auth().signOut().then((res) => {
       const signedOutUser = {
         isSignedIn: false,
@@ -47,11 +47,25 @@ function App() {
       // An error happened.
     });
   }
+  const handleSubmit = () =>{
 
+  }
+  const handleBlur = (e) =>{
+    console.log(e.target.name,e.target.value);
+    if(e.target.name === 'email'){
+      const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
+      console.log(isEmailValid);
+    }
+    if(e.target.name === 'password'){
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber =  /\d{1}/.test(e.target.value);
+      console.log(isPasswordValid && passwordHasNumber);
+    }
+  }
   return (
     <div className="App">
       <h3>Firebase Authentication</h3>
-      <p>using google</p><br/><br/>
+      <p>using google</p><br /><br />
       {
         user.isSignedIn ? <button onClick={handleSignOut}>Sign-out</button> : <button onClick={handleSignIn}>Sign-in</button>
       }
@@ -63,6 +77,16 @@ function App() {
           <img src={user.photo} alt="" />
         </div>
       }
+
+      <h1>Our own Authentication</h1>
+      <form onSubmit={handleSubmit}>
+        <input onBlur={handleBlur} name="email" type="text" placeholder='Your Email Address' required />
+        <br />
+        <input onBlur={handleBlur} type="password" name="password" id="" placeholder='Password' required />
+        <br />
+        <input type="submit" value="Submit"/>
+      </form>
+
     </div>
   );
 }
